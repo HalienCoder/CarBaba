9255af62678bc6b71ebd341417eb6a67c5af059f
HalienCoder <123660335+HalienCoder@users.noreply.github.com>
2023-09-02 16:58:02 +0530

    Add files via upload

---
+++ src/carbaba2/CarBabaMain.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carbaba2;
+
+/**
+ *
+ * @author user
+ */
+public class CarBabaMain {
+    public static void main(String[] args){
+        CarSelector carSel = new CarSelector();
+        
+    }
+    
+}
